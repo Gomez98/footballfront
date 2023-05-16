bb6cd7002b5e6b19527941f5df8df5d3b6204fb4
Gomez98 <gomez9814@hotmail.com>
2023-05-16 13:30:00 -0500

    Finished Gatsby Tutorial Part2

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,10 @@
  */
 module.exports = {
   siteMetadata: {
+    title: `Using Gatsby Head`,
+    description: `Example project for the Gatsby Head API`,
+    twitterUsername: `@gatsbyjs`,
+    image: `/soccer.png`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
 }
